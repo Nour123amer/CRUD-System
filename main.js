7abbfc7bfbd4175e052406839d6220406d55b411
Nour123amer <nouramer295@gmail.com>
2024-11-13 14:36:57 +0200

    cruds

--- main.js
+++ main.js
@@ -112,11 +112,11 @@
 
 // searchProduct();
 
-let productName = document.querySelector('#productName');
-let productPrice = document.querySelector('#productPrice');
-let productCategory = document.querySelector('#productCategory');
-let productDescription = document.querySelector('#productDescription');
-let productImage = document.querySelector('#productImage');
+let productName = document.querySelector('.productName');
+let productPrice = document.querySelector('.productPrice');
+let productCategory = document.querySelector('.productCategory');
+let productDescription = document.querySelector('.productDescription');
+let productImage = document.querySelector('.productImage');
 let addBtn = document.querySelector('#btnAdd');
 let updateBtn = document.querySelector('#btnUpdate');
 let content = document.querySelector('.content');
@@ -138,11 +138,19 @@ if (localStorage.getItem("productList") !== null) {
 }
 
 function addProduct() {
- if(validateName()  &&
-  validatePrice()  &&
-  validateCategory()  &&
-  validateDescription()  &&
-  validateImage())
+ if(
+    validateInputs(productName ," productNameRegex") &&
+    validateInputs(productPrice ," productPriceRegex") &&
+    validateInputs(productCategory ,"productCategoryRegex") &&
+    validateInputs(productDescription ,"productDescriptionRegex") 
+    // validateInputs(productImage ,"productImageRegex") 
+//     validateName()  &&
+//   validatePrice()  &&
+//   validateCategory()  &&
+//   validateDescription()  &&
+//   validateImage()
+
+)
   {
     var productValues = {
         name: productName.value,
@@ -162,7 +170,7 @@ function addProduct() {
 
 function displayProducts() {
     content.innerHTML = "";
-    var productData;
+    var productData="";
     for (var i = 0; i < productList.length; i++) {
         productData += `
     <tr>
@@ -171,9 +179,9 @@ function displayProducts() {
                     <td>${productList[i].price}</td>
                     <td>${productList[i].category}</td>
                     <td>${productList[i].description}</td>
-                    <td><img width="100px" src="" alt=""></td>
+                    <td><img width="100px" src=${productList[i].image} alt=""></td>
                     <td>
-                        <button type="button" onclick="lifttingValuesToInputs(${i})" class="btn btn-outline-warning btn-sm">Update</button>
+                        <button type="button" onclick="liftingValuesToInputs(${i})" class="btn btn-outline-warning btn-sm">Update</button>
                         <button type="button" onclick="deleteProduct(${i})" class="btn btn-outline-danger btn-sm">Delete</button>
                     </td>
                 </tr>
@@ -209,7 +217,7 @@ function deleteProduct(index) {
 function searchProducts() {
     let term = searchInput.value;
     content.innerHTML = "";
-    var productData;
+    var productData="";
     for (var i = 0; i < productList.length; i++) {
 
         if (productList[i].name.toLowerCase().includes(term.toLowerCase())) {
@@ -220,9 +228,9 @@ function searchProducts() {
                     <td>${productList[i].price}</td>
                     <td>${productList[i].category}</td>
                     <td>${productList[i].description}</td>
-                    <td><img width="100px" src="" alt=""></td>
+                    <td><img width="100px" src=${productList[i].image} alt=""></td>
                     <td>
-                        <button type="button" class="btn btn-outline-warning btn-sm">Update</button>
+                        <button type="button"  class="btn btn-outline-warning btn-sm">Update</button>
                         <button type="button" onclick="deleteProduct(${i})" class="btn btn-outline-danger btn-sm">Delete</button>
                     </td>
                 </tr>
@@ -230,24 +238,26 @@ function searchProducts() {
 
         }
     }
-    console.log(productList.name);
+    console.log(productList);
     
     content.innerHTML = productData;
 }
 
 
 
-function lifttingValuesToInputs(indexElement) {
+function liftingValuesToInputs(indexElement) {
     productName.value = productList[indexElement].name;
     productPrice.value = productList[indexElement].price;
     productCategory.value = productList[indexElement].category;
     productDescription.value = productList[indexElement].description;
-    productImage.value = productList[indexElement].image;
+    // productImage.value = productList[indexElement].image;
 
     addBtn.classList.add('d-none');
     updateBtn.classList.remove('d-none');
     index = indexElement;
 }
+
+console.log(index);
 
 function updateProduct() {
     var productValues = {
@@ -255,13 +265,12 @@ function updateProduct() {
         price: productPrice.value,
         category: productCategory.value,
         description: productDescription.value,
-        image: productImage.value,
+        image: productImage.files[0]?.name?`images/products/${productImage.files[0]?.name}`:`images/products/bg.jpeg`
     };
 
     productList.splice(index, 1, productValues);
     localStorage.setItem("productList", JSON.stringify(productList));
-    displayProducts();
-    
+    displayProducts(); 
     clearForm();
 }
 
@@ -273,118 +282,130 @@ addBtn.addEventListener('click', () => {
 
 // validation
 
-function validateName() {
-    var name = productName.value;
-    var nameRegex = /^[A-z][a-z]{3,8}$/;
-    window.onload = function() {
-        errorMsgName.classList.add('d-none');
-        productName.classList.remove('is-invalid');
-    };
+//element => input
 
-    if (nameRegex.test(name) == true) {
-
-        errorMsgName.classList.add('d-none');
-        productName.classList.remove('is-invalid');
-        productName.classList.add('is-valid');
-
-    } else {
-        productName.classList.remove('is-valid');
-        productName.classList.add('is-invalid');
-        errorMsgName.classList.remove('d-none');
+function validateInputs(element ,msgId) {
+    var text = element.value;
+    console.log(text);
+    
+    var regex = {
+        productNameRegex:/^[A-z][a-z]{3,8}$/,
+        productPriceRegex:/^(5|[6-9]|\d{2}|100)$/,
+        productCategoryRegex:/^(Tv|mobile|screens|camera)$/i,
+        productDescriptionRegex: /^[A-z][a-z]{3,20}$/,
+        productImageRegex:/^.{1,}\.(jpg|png|avif|jpeg|svg)$/,
     }
-   return true;
-}
-
-function validatePrice() {
+    var msg = document.getElementById(msgId);
 
     window.onload = function() {
-        errorMsgPrice.classList.add('d-none');
-        productPrice.classList.remove('is-invalid');
-    };
-    var price = productPrice.value;
-    var priceRegex = /^(5|[6-9]|\d{2}|100)$/;
-    if (priceRegex.test(price) == true) {
-
-        errorMsgPrice.classList.add('d-none');
-        productPrice.classList.remove('is-invalid');
-        productPrice.classList.add('is-valid');
-
-    } else {
-        productPrice.classList.remove('is-valid');
-        productPrice.classList.add('is-invalid');
-        errorMsgPrice.classList.remove('d-none');
-    }
-
-    return true;
-
-
-}
-
-function validateCategory() {
-    let errorMsgCategory= document.querySelector('.errorMsgCategory');
-    var category = productCategory.value;
-    var categoryRegex = /^(Tv|mobile|screens|camera)$/i;
-    window.onload = function() {
-        errorMsgCategory.classList.add('d-none');
-        productCategory.classList.remove('is-invalid');
+        msg.classList.add('d-none');
+        element.classList.remove('is-invalid');
     };
 
-    if (categoryRegex.test(category) == true) {
+    if (regex[element.id].test(text) == true) {
 
-        errorMsgCategory.classList.add('d-none');
-        productCategory.classList.remove('is-invalid');
-        productCategory.classList.add('is-valid');
-
-    } else {
-        productCategory.classList.remove('is-valid');
-        productCategory.classList.add('is-invalid');
-        errorMsgCategory.classList.remove('d-none');
-    }
-   return true;
-}
-
-function validateDescription() {
-    let errorMsgDescription= document.querySelector('.errorMsgDescription');
-    var description = productDescription.value;
-    var descriptionRegex = /^[A-z][a-z]{3,20}$/;
-    window.onload = function() {
-        errorMsgDescription.classList.add('d-none');
-        productDescription.classList.remove('is-invalid');
-    };
-
-    if (descriptionRegex.test(description) == true) {
-
-        // errorMsgDescription.classList.add('d-none');
-        productDescription.classList.remove('is-invalid');
-        productDescription.classList.add('is-valid');
+        msg.classList.add('d-none');
+        element.classList.remove('is-invalid');
+        element.classList.add('is-valid');
 
     } else {
-        productDescription.classList.remove('is-valid');
-        productDescription.classList.add('is-invalid');
-        errorMsgDescription.classList.remove('d-none');
+        element.classList.remove('is-valid');
+        element.classList.add('is-invalid');
+        msg.classList.remove('d-none');
     }
-   return true;
+   
 }
 
-function validateImage() {
-    let errorMsgImage= document.querySelector('.errorMsgImage');
-    var image = productImage.value;
-    var imageRegex = /^.{1,}\.(jpg|png|avif|jpeg|svg)$/;
-    window.onload = function() {
-        errorMsgImage.classList.add('d-none');
-        productImage.classList.remove('is-invalid');
-    };
+// function validatePrice() {
 
-    if (imageRegex.test(image) == true) {
+//     window.onload = function() {
+//         errorMsgPrice.classList.add('d-none');
+//         productPrice.classList.remove('is-invalid');
+//     };
+//     var price = productPrice.value;
+//     var priceRegex = /^(5|[6-9]|\d{2}|100)$/;
+//     if (priceRegex.test(price) == true) {
 
-        errorMsgImage.classList.add('d-none');
-        productImage.classList.remove('is-invalid');
-        productImage.classList.add('is-valid');
+//         errorMsgPrice.classList.add('d-none');
+//         productPrice.classList.remove('is-invalid');
+//         productPrice.classList.add('is-valid');
 
-    } else {
-        productImage.classList.remove('is-valid');
-        productImage.classList.add('is-invalid');
-        errorMsgImage.classList.remove('d-none');
-    }
-   return true;
-}
+//     } else {
+//         productPrice.classList.remove('is-valid');
+//         productPrice.classList.add('is-invalid');
+//         errorMsgPrice.classList.remove('d-none');
+//     }
+
+//     return true;
+
+
+// }
+
+// function validateCategory() {
+//     let errorMsgCategory= document.querySelector('.errorMsgCategory');
+//     var category = productCategory.value;
+//     var categoryRegex = /^(Tv|mobile|screens|camera)$/i;
+//     window.onload = function() {
+//         errorMsgCategory.classList.add('d-none');
+//         productCategory.classList.remove('is-invalid');
+//     };
+
+//     if (categoryRegex.test(category) == true) {
+
+//         errorMsgCategory.classList.add('d-none');
+//         productCategory.classList.remove('is-invalid');
+//         productCategory.classList.add('is-valid');
+
+//     } else {
+//         productCategory.classList.remove('is-valid');
+//         productCategory.classList.add('is-invalid');
+//         errorMsgCategory.classList.remove('d-none');
+//     }
+//    return true;
+// }
+
+// function validateDescription() {
+//     let errorMsgDescription= document.querySelector('.errorMsgDescription');
+//     var description = productDescription.value;
+//     var descriptionRegex = /^[A-z][a-z]{3,20}$/;
+//     window.onload = function() {
+//         errorMsgDescription.classList.add('d-none');
+//         productDescription.classList.remove('is-invalid');
+//     };
+
+//     if (descriptionRegex.test(description) == true) {
+
+//         // errorMsgDescription.classList.add('d-none');
+//         productDescription.classList.remove('is-invalid');
+//         productDescription.classList.add('is-valid');
+
+//     } else {
+//         productDescription.classList.remove('is-valid');
+//         productDescription.classList.add('is-invalid');
+//         errorMsgDescription.classList.remove('d-none');
+//     }
+//    return true;
+// }
+
+// function validateImage() {
+//     let errorMsgImage= document.querySelector('.errorMsgImage');
+//     var image = productImage.value;
+//     var imageRegex = /^.{1,}\.(jpg|png|avif|jpeg|svg)$/;
+//     window.onload = function() {
+//         errorMsgImage.classList.add('d-none');
+//         productImage.classList.remove('is-invalid');
+//     };
+
+//     if (imageRegex.test(image) == true) {
+
+//         errorMsgImage.classList.add('d-none');
+//         productImage.classList.remove('is-invalid');
+//         productImage.classList.add('is-valid');
+
+//     } else {
+//         productImage.classList.remove('is-valid');
+//         productImage.classList.add('is-invalid');
+//         errorMsgImage.classList.remove('d-none');
+//     }
+//    return true;
+// }
